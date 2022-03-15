0eab2fa9a687c1fe0064a810829f3d7010ce4dd5
root <root@qpumpkin.localdomain>
2022-03-16 02:55:05 +0800

    refactor type

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,66 +1,101 @@
-import React from 'react';
-import org_data from "./data/orgs.json";
-import member_data from './data/members.json';
-import { OrgCard, OrgInterface, MemberInterface } from './components/OrgCard';
-import { WiredCard, WiredButton } from 'react-wired-elements';
+import React from "react";
+import orgData from "./data/orgs.json";
+import memberData from "./data/members.json";
+import { OrgCard, Org, Member } from "./components/OrgCard";
+import { WiredCard, WiredButton } from "react-wired-elements";
 
 const App: React.FC = () => {
-  const [org_state, setOrgState] = React.useState(org_data as OrgInterface[]);
-  const [member_state, setMemberState] = React.useState(member_data);
-  const membersMap: Map<string, MemberInterface> = new Map(member_state.map((member: MemberInterface) => [member.id, member]));
-  const orgsMap: Map<string, OrgInterface> = new Map(org_state.map((org: OrgInterface) => [org.id, org]))
-  const findChildOrg = (parent: OrgInterface) => org_state.filter(org => org.parent === parent.id);
-  const map2Member = (members: string[]) => members.map(membersMap.get.bind(membersMap)).filter(Boolean) as MemberInterface[];
-  const memberChange = (member: MemberInterface) => {
+  const [orgState, setOrgState] = React.useState<Org[]>(orgData);
+  const [memberState, setMemberState] = React.useState<Member[]>(memberData);
+  
+  const membersMap: Map<string, Member> = new Map(
+    memberState.map((member) => [member.id, member])
+  );
+  const orgsMap: Map<string, Org> = new Map(
+    orgState.map((org) => [org.id, org])
+  );
+
+  const findChildOrg = (parent: Org) =>
+    orgState.filter((org) => org.parent === parent.id);
+
+  const map2Member = (members: string[]) =>
+    members.map(membersMap.get.bind(membersMap)).filter(Boolean) as Member[];
+
+  const memberChange = (member: Member) => {
     const id = member.id;
-    const idx = member_state.findIndex(m => m.id === id);
-    const new_member_state = [...member_state];
+    const idx = memberState.findIndex((m) => m.id === id);
+    const new_member_state = [...memberState];
     new_member_state[idx] = member;
     membersMap.set(id, member);
     setMemberState(new_member_state);
-  }
-  const orgChange = (org: OrgInterface) => {
+  };
+
+  const orgChange = (org: Org) => {
     const id = org.id;
-    const idx = org_state.findIndex(m => m.id === id);
-    console.log(org_state[idx].name, org.name)
-    const new_org_state = [...org_state];
+    const idx = orgState.findIndex((m) => m.id === id);
+    console.log(orgState[idx].name, org.name);
+    const new_org_state = [...orgState];
     new_org_state[idx] = org;
     orgsMap.set(id, org);
     setOrgState(new_org_state);
-  }
-
+  };
 
   return (
-    <WiredCard className='full' elevation={1}>
-      <h1>Org Management</h1>
-      <div className='w-full'>
-        {org_state.filter(org => org.parent === null).map((org: OrgInterface) => <OrgCard data={org} findChildOrg={findChildOrg} map2Member={map2Member} key={org.id} memberChange={memberChange} orgChange={orgChange} />)}
+    <WiredCard className="w-full p-4" elevation={1}>
+      <h4 className="text-5xl my-5">Org Management</h4>
+      <div>
+        {orgState
+          .filter((org) => org.parent === null)
+          .map((org: Org) => (
+            <OrgCard
+              data={org}
+              findChildOrg={findChildOrg}
+              map2Member={map2Member}
+              key={org.id}
+              memberChange={memberChange}
+              orgChange={orgChange}
+            />
+          ))}
       </div>
       <div>
-        <WiredButton onClick={() => {
-          const copy = [...org_state];
-          copy.push({
-            id: String(Date.now()),
-            name: '',
-            type: "organization",
-            representation: "",
-            parent: null,
-            members: [],
-          })
-          setOrgState(copy);
-        }} className='text-4xl'> + </WiredButton> 
+        <WiredButton
+          onClick={() => {
+            const copy = [...orgState];
+            copy.push({
+              id: String(Date.now()),
+              name: "",
+              type: "organization",
+              representation: "",
+              parent: null,
+              members: [],
+            });
+            setOrgState(copy);
+          }}
+          className="text-4xl"
+        >Add
+        </WiredButton>
       </div>
-      <div className='flex justify-end'>
-        <WiredButton className='text-2xl mr-4' onClick={() => {
-          setMemberState(member_data);
-          setOrgState(org_state);
-        }}>Cancel</WiredButton>
-        <WiredButton onClick={() => {
-          console.log('当前值为', member_state, org_state)
-        }} className='text-2xl'>Save</WiredButton> 
+      <div className="flex justify-end">
+        <WiredButton
+          className="text-2xl mr-4"
+          onClick={() => {
+            setMemberState(memberData);
+            setOrgState(orgState);
+          }}
+        >
+          Cancel
+        </WiredButton>
+        <WiredButton
+          onClick={() => {
+            console.log("当前值为", memberState, orgState);
+          }}
+          className="text-2xl"
+        >
+          Save
+        </WiredButton>
       </div>
     </WiredCard>
   );
-}
+};
 
 export default App;
